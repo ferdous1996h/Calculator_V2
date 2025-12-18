bbaa1ebef221bddd4e7ad8dedc3518eebee0426a
Md. Ferdous Hasan <138438377+ferdous1996h@users.noreply.github.com>
2025-12-18 16:07:25 +0600

    Major bug fixed

--- script.js
+++ script.js
@@ -1,78 +1,43 @@
 'use strict';
-const dis_value = document.querySelector('.display');
-const reset = document.querySelector('.rst');
-const back = document.querySelector('.bk');
-const operators = document.querySelector('.operator');
-const btn = document.querySelectorAll('.num_btn');
+const dis = document.querySelector('.display');
+const btn = document.querySelectorAll('button');
 
-let num1 = [];
-let result1 = 0;
-let result2 = 0;
-let lastOp = '';
-let finalR='0';
-function calc(a, b, ope) {
-  switch (ope) {
-    case '÷': return a / b;
-    case '×': return a * b;
-    case '-': return a - b;
-    case '+': return a + b;
-  }
-}
-btn.forEach(b => b.addEventListener('click', (e) => {
-  const textC = b.textContent;
-  const regexNum = new RegExp('^[0-9]$');
-  if (regexNum.test(textC)) {
-    num1.push(textC);
-    finalR = num1.join('');
-  } 
-  if (b.classList.contains('rst')) {
-    num1 = []
-    finalR = '0';
-    btn.forEach(a=>a.removeAttribute('disabled'))
-  }
-  if (b.textContent === '⟵') {
-    if (num1.length <2 ) {
-      num1.pop();
-      finalR = 0;
-    } else if (num1.length >1) {
-      num1.pop();
-      finalR = num1.join('');
+btn.forEach(tap =>
+  tap.addEventListener('click', () => {
+    const disV = dis.textContent.trim();
+    const tapV = tap.textContent.trim();
+
+    if (tap.classList.contains('rst')) {
+      dis.textContent = '0';
+      return;
+    }
+    if (tap.classList.contains('bk')) {
+      if (disV.length <= 1) {
+        dis.textContent = '0';
+      } else {
+        dis.textContent = disV.slice(0, -1);
+      }
+      return;
+    }
+    if (tap.classList.contains('result')) {
+      const finalR = disV.replace(/×/g, '*').replace(/÷/g, '/');
+      const finalRValue = new Function('return ' + finalR)();
+      dis.textContent = Number.isInteger(finalRValue)
+        ? finalRValue
+        : finalRValue.toFixed(4);
+      return;
+    }
+    const lastOp = disV.slice(-1);
+    const operator = ['-', '+', '÷', '×'];
+    if (operator.includes(lastOp) && operator.includes(tapV)) {
+      dis.textContent = disV.slice(0, -1) + tapV;
+      return;
     }
 
-  }
-  if (b.textContent === '÷') {
-    result1 = parseInt(num1.join(''));
-    num1=['0']
-    finalR = num1.join('');
-    lastOp = '÷';
-  }
-  if (b.textContent === '+') {
-    result1 = parseInt(num1.join(''));
-    num1=['0']
-    finalR = num1.join('');
-    lastOp = '+';
-  }
-  if (b.textContent === '-') {
-    result1 = parseInt(num1.join(''));
-    num1=['0']
-    finalR = num1.join('');
-    lastOp = '-';
-  }
-  if (b.textContent === '×') {
-    result1 = parseInt(num1.join(''));
-    num1 = ['0'];
-    finalR = num1.join('');
-    lastOp = '×';
-  }
-  if (b.textContent === '=') {
-    result2 = parseInt(num1.join(''));
-    finalR = calc(parseInt(result1), parseInt(result2), lastOp);
-    result1 = finalR;
-    btn.forEach(a => {
-      if (!a.classList.contains('rst')) {
-        a.setAttribute('disabled', '');
-      }
-    })
-  }
-  dis_value.textContent= finalR;
-}))
+    if (disV === '0') {
+      dis.textContent = tapV;
+    } else {
+      dis.textContent += tapV;
+    }
+  })
+);
